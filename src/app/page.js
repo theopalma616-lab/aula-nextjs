70a7ea20c83b6fda2455f0a805c815c8e67551f8
Palma <theopalma616@gmail.com>
2026-02-27 11:43:24 -0300

    aula 27-02

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-const Card = () => {
-  return (
-    <div>
-      <h3>Aula introdução</h3>
-      <h3>Aula Estilização</h3>
-      <h3>Aula Componentes</h3>
-    </div>
-  );
-}
-
 function Home() {
   return (
     <div>

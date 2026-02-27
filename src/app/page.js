32ff7d26dbdae64e1c973e51fbd1ece1c8d3ece6
Palma <theopalma616@gmail.com>
2026-02-27 11:05:40 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+const Card = () => {
+  return (
+    <div>
+      <h3>Aula introdução</h3>
+      <h3>Aula Estilização</h3>
+      <h3>Aula Componentes</h3>
+    </div>
+  );
+}
+
+function Home() {
   return (
     <div>
       <h1>React com Next JS</h1>
     </div>
   );
 }
+
+
+export default Home;
+

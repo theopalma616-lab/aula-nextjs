d6036b5077b9102dde49bb40e48d034213b5260f
Palma <theopalma616@gmail.com>
2026-02-27 13:48:09 -0300

    aula css

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ function Home() {
   return (
     <div>
       <h1>React com Next JS</h1>
+      <h2>Aula de introdução</h2>
+      <h3>leiaute e estilização</h3>
+      <p>Nestea aula iremos entender o uso de CSS global e local</p>
     </div>
   );
 }

060b4d5fba908af0247baf92eb85b2367ffdffb2
Palma <theopalma616@gmail.com>
2026-03-13 13:50:26 -0300

    aula atividade1

---
+++ src/app/atividades/atv-01/page.jsx
@@ -0,0 +1,9 @@
+function Atividade01 () {
+    return(
+        <div className={styles.containerParagrafo}>
+            <h1>omponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com React</h1>
+        </div>
+    )
+}
+
+export default Atividade01
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Link from 'next/link';
 import styles from "./page.module.css";
 
 function Home() {
@@ -6,6 +6,8 @@ function Home() {
     <div className={styles.containerHome}>
       <div className={styles.containerListas}>
         <h1>Exemplos</h1>
+        <Link href="/exemplos/ex-01">Exemplo 1 </Link> 
+        <Link href="/exemplos/ex-02">Exemplo 2 </Link>
       </div>
       <div className={styles.containerListas}>
         <h1>Atividades</h1>

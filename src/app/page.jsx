da7b789f39731fa808d0b82923aa9f28f1047daf
Palma <theopalma616@gmail.com>
2026-03-13 11:46:15 -0300

    aula7tabichado

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,16 +3,12 @@ import styles from "./page.module.css";
 
 function Home() {
   return (
-    <div className={styles.containerExemplo}>
-      <h1>React com Next JS</h1>
-      <h2>Aula de introdução</h2>
-      <h3>Leiaute e Estilização</h3>
-      <p>Nesta aula iremos entender o uso de CSS global e local.</p>
-      <label>Exemplo Label</label>
-      <small>Exemplo Small</small>
-      <div className={styles.conteinerExPosicao}>
-        <div className={styles.divVermelha}></div>
-        <div className={styles.divVerde}></div>
+    <div className={styles.containerHome}>
+      <div className={styles.containerListas}>
+        <h1>Exemplos</h1>
+      </div>
+      <div className={styles.containerListas}>
+        <h1>Atividades</h1>
       </div>
     </div>
   );

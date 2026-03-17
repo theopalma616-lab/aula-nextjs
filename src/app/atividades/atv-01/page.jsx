b20c63d87f6495f91ee70063c269213d2fa326b5
Palma <theopalma616@gmail.com>
2026-03-17 08:58:31 -0300

    atv01

--- src/app/atividades/atv-01/page.jsx
+++ src/app/atividades/atv-01/page.jsx
@@ -1,21 +1,11 @@
 import Link from "next/link";
 import style from "./page.module.css";
-import Image from "next/image";
-import salsicha from './img/cachorro.webp';
 
 export default atividade01;
 
 
 function atividade01(){
     return(
-        <div className={style.stlG}>
-            <h1>atividade 1</h1>
-            <div className={style.stl1}>
-                <h1>apenas um cachorro salsicha</h1>
-                <Image src={salsicha}></Image>
-                <Link href="/">voltar</Link>
-            </div>
-               
-        </div>
+        <div></div>
     )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,6 +11,7 @@ function Home() {
       </div>
       <div className={styles.containerListas}>
         <h1>Atividades</h1>
+        <Link href="/atividades/atv-01"> Atividade 1</Link>
       </div>
     </div>
   );

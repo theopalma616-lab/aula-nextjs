9759b5f667d28d5ce09f99bed874ee7e1ab4feb8
Palma <theopalma616@gmail.com>
2026-03-17 08:49:44 -0300

    atividade 01

--- src/app/atividades/atv-01/page.jsx
+++ src/app/atividades/atv-01/page.jsx
@@ -1,9 +1,21 @@
-function Atividade01 () {
+import Link from "next/link";
+import style from "./page.module.css";
+import Image from "next/image";
+import salsicha from './img/cachorro.webp';
+
+export default atividade01;
+
+
+function atividade01(){
     return(
-        <div className={styles.containerParagrafo}>
-            <h1>omponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com Reactomponente básico para renderizar tela com React</h1>
+        <div className={style.stlG}>
+            <h1>atividade 1</h1>
+            <div className={style.stl1}>
+                <h1>apenas um cachorro salsicha</h1>
+                <Image src={salsicha}></Image>
+                <Link href="/">voltar</Link>
+            </div>
+               
         </div>
     )
 }
-
-export default Atividade01

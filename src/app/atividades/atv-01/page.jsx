4566272531178c0811f84dcd4ba24639fb907b02
Palma <theopalma616@gmail.com>
2026-03-20 11:23:37 -0300

    exe1

--- src/app/atividades/atv-01/page.jsx
+++ src/app/atividades/atv-01/page.jsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import style from "./page.module.css";
-
+import image from "next/image"
+import dautin from ""
 export default atividade01;
 
 
 function atividade01(){
     return(
-        <div></div>
+        <div className="text">
+            <h1>Dalthon</h1>
+    
+        </div>
+
     )
 }

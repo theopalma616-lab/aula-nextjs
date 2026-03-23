8a07a032b3bfc3d921e719ec930957256b15444c
Palma <theopalma616@gmail.com>
2026-03-23 10:43:37 -0300

    atividade 2 feita

---
+++ src/app/atividades/atv-02/page.jsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useState } from "react";
+import styles from "./page.module.css"
+
+export default function Exemplo03() {  
+
+    const [Som, setSom] = useState(0);
+    const [Sub, setSub] = useState(0);
+
+    function handleSom () {
+        setSom(Som + 1);
+    }
+
+    function handleSub () {
+        setSub(Sub - 1);
+    }
+
+    const total = Som + Sub;
+
+    return (
+        <div className={styles.container}>
+            <label>{`Contador de cliques: ${total}`}</label>    
+            <button onClick={() => handleSom()} className={styles.button1}>+1</button> <br/>
+            <button onClick={() => handleSub()} className={styles.button2}>-1</button>
+        </div>
+    );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,6 +13,7 @@ function Home() {
       <div className={styles.containerListas}>
         <h1>Atividades</h1>
         <Link href="/atividades/atv-01"> Atividade 1</Link>
+        <Link href="/atividades/atv-02"> Atividade 2</Link>
       </div>
     </div>
   );

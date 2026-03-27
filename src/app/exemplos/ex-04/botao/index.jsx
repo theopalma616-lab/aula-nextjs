02a710015269345fa16f80ba8f8f816b021478e9
THEO PALMA TAVARES DE MATTOS FERNANDES <theo.fernandes>
2026-03-27 11:43:53 -0300

    atividade 3 inicio

---
+++ src/app/exemplos/ex-04/botao/index.jsx
@@ -0,0 +1,15 @@
+import styles from './index.module.css';
+
+function Botao({ texto, aoClicar, acao }) {
+    return (
+        <button
+            className={`${styles.botao} ${acao === '+' ? styles.mais : styles.menos}`}
+            // className={styles.botao + ' ' + acao === '+' ? styles.mais : styles.menos}
+            onClick={aoClicar}
+        >
+            {texto}
+        </button>
+    );
+}
+
+export default Botao;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,11 +9,13 @@ function Home() {
         <Link href="/exemplos/ex-01">Exemplo 1 </Link> 
         <Link href="/exemplos/ex-02">Exemplo 2 </Link>
         <Link href="/exemplos/ex-03">Exemplo 3 </Link>
+        <Link href="/exemplos/ex-04">Exemplo 4 </Link>
       </div>
       <div className={styles.containerListas}>
         <h1>Atividades</h1>
         <Link href="/atividades/atv-01"> Atividade 1</Link>
         <Link href="/atividades/atv-02"> Atividade 2</Link>
+        <Link href="/atividades/atv-03"> Atividade 3</Link>
       </div>
     </div>
   );
